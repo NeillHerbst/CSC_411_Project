2853978dd38758d5758cd4a8a31172ef258b933d
Ruan Williams <r.williams.sg@gmail.com>
2016-03-13 14:06:30 +0200

    Changed file names and added gridplot layout for scatter and marginal histogram

--- route.py
+++ route.py
@@ -10,7 +10,7 @@ import numpy as np
 from flask import Flask, render_template
 
 from bokeh.plotting import figure, show, gridplot
-from bokeh.models import ColumnDataSource, CustomJS, VBoxForm, HBox
+from bokeh.models import ColumnDataSource, CustomJS
 from bokeh.models.tools import BoxSelectTool
 #from bokeh.io import vform
 from bokeh.embed import components
@@ -23,7 +23,7 @@ y = 100*np.random.rand(N)
 source = ColumnDataSource(data=dict(x=x,y=y))
 
 #Interaction tools
-TOOLS = 'crosshair, help, reset, resize'
+TOOLS = 'box_select,crosshair, help, reset'
 
 # Figure plotting function
 def make_figure():
@@ -41,18 +41,21 @@ def make_figure():
     
     #Create marginal histogram for y-axis data density
     #set up figure
-#    hist_plot = figure(plot_height=400, plot_width=200, tools="")
+    hist_plot = figure(plot_height=400, plot_width=200)
     #get histogram data 
-#    hist, edges = np.histogram(y, density=True, bins=10)
+    hist, edges = np.histogram(y, density=True, bins=10)
     #add create and add data for histograme
-#    hist_plot.quad(top=edges[1:], bottom=edges[:-1], left=0, right=hist,
-#                   fill_color="#036564", line_color="#033649")
+    hist_plot.quad(top=edges[1:], bottom=edges[:-1], left=0, right=hist,
+                   fill_color="#036564", line_color="#033649")
     
     #add mean line to histogram data (smooth)
     #(....)
     
-    show(scat_plot)
-    return scat_plot #need to return the layout
+    #create plot layout
+    layout = gridplot([[scat_plot,hist_plot]])
+
+    show(layout)
+    return layout #need to return the layout
         
 # Calling plotting Function
 p = make_figure()
@@ -67,9 +70,9 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
     
-@app.route('/scatter_plot')
+@app.route('/plot')
 def plot():
-   return render_template('scatter_plot.html', script=script, div=div)
+   return render_template('plot.html', script=script, div=div)
     
 if __name__ == '__main__':
     app.run(debug=True)
