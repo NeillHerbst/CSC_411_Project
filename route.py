3f3468b6764a29b6f0452074d92b85fdc06a59ff
Ruan Williams <r.williams.sg@gmail.com>
2016-03-30 00:08:03 +0200

    Added stock data read from csv file, and changed the selector box to select data on mouse move

--- route.py
+++ route.py
@@ -9,7 +9,7 @@ import numpy as np
 
 from flask import Flask, render_template
 
-from bokeh.plotting import figure, show, gridplot
+from bokeh.plotting import figure, show, gridplot, output_server
 from bokeh.models import ColumnDataSource, CustomJS
 from bokeh.models.tools import BoxSelectTool
 #from bokeh.io import vform
@@ -23,7 +23,7 @@ y = 100*np.random.rand(N)
 source = ColumnDataSource(data=dict(x=x,y=y))
 
 #Interaction tools
-TOOLS = 'box_select,crosshair, help, reset'
+TOOLS = "box_select,crosshair, help, reset"
 
 # Figure plotting function
 def make_figure():
@@ -37,7 +37,10 @@ def make_figure():
     scat_plot.add_tools(BoxSelectTool(dimensions=["width"]))
     
     #add data to scatter plot
-    scat_plot.scatter('x','y', source=source,size=5)
+    scat_plot.scatter("x","y", source=source,size=5)
+    
+    #Create reggression line (slectable tool)
+    
     
     #Create marginal histogram for y-axis data density
     #set up figure
@@ -47,13 +50,29 @@ def make_figure():
     #add create and add data for histograme
     hist_plot.quad(top=edges[1:], bottom=edges[:-1], left=0, right=hist,
                    fill_color="#036564", line_color="#033649")
+    #styleing histograms               
+    hist_plot.xaxis.axis_label = ""
+    hist_plot.yaxis.axis_label = ""
+    hist_plot.axis.visible = None
     
-    #add mean line to histogram data (smooth)
-    #(....)
+    #add probibility density function line to histogram data (smooth)
+    #calculate arithmetic mean
+    mu = np.mean(y)
+    #calculating standard deviation
+    sigma = np.std(y)
+    #calculating normal distribution (probability density function)
+    y_span = np.linspace(np.min(y),np.max(y),np.size(y))
+    nd = 1/(2*np.pi*sigma)*np.exp(-(y_span - mu)**2/(2*sigma**2))
+    #construct line
+    hist_plot.line(nd,y_span,line_color="#D95B43", line_width=1,alpha=0.7)
+    cent = [(edges[1] - edges[0])/2]
+    for i in range(np.size(edges)):          
+          cent.append((edges[1] - edges[0])/2 + edges[i])   
     
+    hist_plot.line(hist,cent,line_color="red", line_width=1,alpha=1)
     #create plot layout
     layout = gridplot([[scat_plot,hist_plot]])
-
+    output_server('plot')
     show(layout)
     return layout #need to return the layout
         
