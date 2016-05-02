12d4b0313e1257360a29fd5f11f745578c8048c1
Ruan Williams <r.williams.sg@gmail.com>
2016-05-02 15:45:32 +0200

    Added javascript histogram interaction event

--- static/js/hist_update.js
+++ static/js/hist_update.js
@@ -1,34 +1,47 @@
-function UpdateHistogram(cb_obj, hist_data) {
+function UpdateHistogram(cb_obj, hist_data, s_data) {
     /*cb_obj is the source data (data that is being selected)*/
     var inds = cb_obj.get('selected')['1d'].indices; /*get the indices of the selected dataa*/
     var d1 = cb_obj.get('data'); /*push source data to d*/
     var d2 = [];
     var data = hist_data.get('data');
+    var source_data = s_data.get('data');
+    var bin_thresholds = [];
 
     d2['x'] = [];
     d2['y'] = [];
 
-    data['x'] = [];
-    data['y'] = [];
+    data['right'] = [];
 
-    if (inds.length == 0) {return;}
+    if (inds.length == 0) {
+        d2['x'] = [];
+        d2['y'] = [];
+    }
     for (i=0; i<inds.length; i++) {
         d2['x'].push(d1['x'][inds[i]]);
         d2['y'].push(d1['y'][inds[i]]);
     };
 
     /*use d3 to calculate the histogram*/
+    bin_dx = (Math.max(...source_data['y']) - Math.min(...source_data['y']))/20; //20 is the number of bins
+    
+    for (i=0; i<21;i++) {
+        if (bin_thresholds.length == 0) {
+            bin_thresholds.push(Math.min(...source_data['y']));
+        } else {bin_thresholds.push(bin_thresholds[i-1] + bin_dx)};
+        
+    };
+     
     var histogram = d3.layout.histogram()
                     .frequency(false)
-                    .bins(20) 
+                    .bins(bin_thresholds) 
                     (d2['y']);
-                    
-    for (i=0; i<20;i++) {
-        data['x'].push(0)
-        data['y'].push(histogram[i].y)    
-    }
+    console.log(histogram)
 
+    for (i=0; i<20;i++) {
+        data['right'].push((histogram[i].y)/(histogram[i].dx));   
+    };
+    
     hist_data.trigger('change');
-}
+};
 
 /*histogram[i].y*/
